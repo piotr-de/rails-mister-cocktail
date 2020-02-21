8a37eef566a37e39b71dcdbe016251ec7bb47165
Piotr Damer <piotrd@czuby.net>
2020-02-21 11:35:39 +0000

    Added the root path

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,6 @@
 url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
 ingredients = JSON.parse(RestClient.get(url))
 
-
-
 ingredients["drinks"].each do |ingredient|
   Ingredient.create!(name: ingredient["strIngredient1"])
 end
